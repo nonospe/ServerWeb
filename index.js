20749ab27f500efca20aafdd32a3b5661dee0d91
Antonio Jose Jimenez Segovia <antoniojosejimenezsegovia@gmail.com>
2020-03-03 22:57:55 +0000

    terminamos el punto 13

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
+const express = require("express");
+
+var app = express();
+app.get("/time", (request, response) => {
+	response.send("<html>"+new Date()+"</html>");
+});
+app.listen(80);
+
 console.log(new Date());
